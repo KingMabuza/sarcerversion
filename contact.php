c77ba88445b6fc8ea7b59d153fa81473696e6d3f
Amal <amal@amallevi.com>
2020-11-29 18:18:39 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $mailTo = 'info@sarcer.com';
+    $headers = 'From: '.$email;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sarcer - Professional Web Development Studio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <link rel="stylesheet" href="global.css">
+</head>
+<body>
+
+    <div id="intro">
+        <nav class="transparent z-depth-0">
+            <div class="nav-wrapper container">
+              <a href="" class="brand-logo left"><span id="head1">Sarcer</span></a>
+            </div>
+        </nav>
+        <h6 id="head2" class="container"><b id="typed"></b></h6>
+    </div>
+        <div class="container">
+            <h6 class="center">Welcome,thank you for visiting our website</h6>
+            <h5 class="center">Our <span>Passion</span> is the <span>success</span> of your projects</h5>
+            <p class="center">Sarcer,is a digital studio based in Johannesburg,South Africa.We are an agency solely dedicated to setting the course of both small and large businesses on the path of better achievements. Our team of experts work together, seamlessly combining individual expertise to bring strategies to fruition. </p>
+            <p class="center">We walk businesses through the customer journey, allowing our clients to understand their clientele better. Sarcer is not focused on merely solving a problem but on providing long-term solutions</p>
+            <h5 class="center">Our Services</h5></p>
+            <div class="center">
+                <div class="row">
+                <div class="col s12">
+                    <div data-aos="zoom-in-up">
+                        <div class="card center z-depth-4">
+                            <div class="card-content">
+                              <span class="card-title">Web Development</span>
+                              <p>Your website is the one-stop shop for your brand. No matter what your business or profession, a professionally set up website can be the ticket to increased revenue, goodwill among customers and your target market, small, medium, or large, your business needs a well crafted website.</p>
+                              <p>As Sarcer, we work with you to identify your problem points, this allows us to create a product that not only feels intuitive but actually solves your problem as a long-term solution.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            </div>
+
+            <div class="center">
+                <div class="row">
+                <div class="col s12">
+                    <div data-aos="zoom-in-up">
+                      <div class="card center z-depth-4">
+                        <div class="card-content">
+                          <span class="card-title">App Development</span>
+                          <p>As Sarcer we aim to develop high-quality iOS apps and Android mobile applications. Intuitive, engaging and scalable, are our three goals when building an application.</p>
+                          <p>We utilize your service offering and create a high-performing native application</p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="center">
+                <div class="row">
+                <div class="col s12">
+                    <div data-aos="zoom-in-up">
+                      <div class="card center z-depth-4">
+                        <div class="card-content">
+                          <span class="card-title">UI/UX Design</span>
+                            <p>We realize just how much an impact UI/UX has, it is the difference between confidence in your product & brand and weak brand strength. Well designed UI/UX prompts your clientele to engage with your offering. As Sarcer we create uniquely beautiful and functional work grounded in results-oriented best practices.</p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="center">
+                <div class="row">
+                <div class="col s12">
+                    <div data-aos="zoom-in-up">
+                        <div class="card center z-depth-4">
+                            <div class="card-content">
+                              <span class="card-title">Digital Marketing</span>
+                              <p>Do you have a website but not seeing any increase in leads or sales?
+    Well, it’s high time Sarcer helped you to optimize your website. We provide complete digital marketing solutions including online marketing, lead generation, SEO and PPC, email marketing, content management, and social media marketing for your business growth.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="center">
+                <div class="row">
+                <div class="col s12">
+                  <div data-aos="zoom-in-up">
+                  <div class="card center z-depth-4">
+                    <div class="card-content">
+                      <span class="card-title">Business Intelligence</span>
+                      <p>As Sarcer we provide business intellgence as a way to enable you to transform your large amount of data into actionable insights. Statistically, a majority of business intelligence initiatives are not successful,this is as a result of poor data quality and management to not adapting reporting and analysis as the business matures.
+Working with a company like Sarcer not only mitigates this risk, but more so enables you to achieve your organizational goals.</p>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+        <h5 class="center">Ready To Work?</h5>
+            <h6 class="center">Tell us about your project, let us help you achieve the success you envisioned</h6>
+                <div class="row">
+                    <form class="col s12" method="post" action="contact.php">
+                      <div class="row">
+                        <div class="input-field col s6">
+                          <i class="material-icons prefix">account_circle</i>
+                          <input id="name" type="text" class="validate" name="name">
+                          <label for="name">Full Name</label>
+                        </div>
+                        <div class="input-field col s6">
+                          <i class="material-icons prefix">email</i>
+                          <input id="email" type="email" class="validate" name="email">
+                          <label for="email">Email</label>
+                        </div>
+                        <div class="input-field col s6">
+                          <i class="material-icons prefix">topic</i>
+                          <input id="subject" type="text" class="validate" name="subject">
+                          <label for="subject">Subject</label>
+                        </div>
+                       <div class="input-field col s6">
+                          <i class="material-icons prefix">call</i>
+                          <input id="phone" type="text" class="validate" name="phone">
+                          <label for="phone">Phone</label>
+                        </div>
+                       <div class="input-field col s12">
+                          <i class="material-icons prefix">message</i>
+                          <input id="message" type="text" class="validate" name="message">
+                          <label for="message">Message</label>
+                        </div>
+                      </div>
+                      <button class="btn transparent" id="btn">Send</button>
+                    </form>
+            </div>
+            <a href="mailto:info@sarcer.com" id="mail">info@sarcer.com</a>
+            <footer class="center">Copyright &copy;2020 <span><a href="">Sarcer</a></span></footer>
+            </div>
+
+<script>
+new TypeIt("#typed", {
+    strings: ["Launching Your Vision Into Success"],
+  speed: 125,
+  waitUntilVisible: true,
+  lifeLike: true,
+}).go();
+
+AOS.init();
+</script>
+</body>
+</html>
