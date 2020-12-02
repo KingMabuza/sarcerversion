dba8282aa641c61d3aade36673f4c1c3a6b57855
Amal <amal@amallevi.com>
2020-12-02 02:03:53 +0200

    first commit

--- contact.php
+++ contact.php
@@ -1,6 +1,9 @@
 <?php
 
     if (isset($_POST['submit'])){
+        ini_set("SMTP", "mail.zoho.com");
+        ini_set("sendmail_from", "king@sarcer.com");
+        ini_set("smtp_port", 587);
         $name = $_POST['name'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
